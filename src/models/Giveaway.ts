771d770e6efb0389a3d81ec5a28f9c401638adf9
Bailey Townsend <baileytownsend2323@gmail.com>
2022-11-14 05:01:25 +0000

    Found the debugging logs for discord

--- src/models/Giveaway.ts
+++ src/models/Giveaway.ts
@@ -1,10 +1,10 @@
 export type Giveaway = {
-    stillRunning?: Boolean;
-    winnerIds?: Array<string>;
-    participants?: Array<string>
-    messageId: string;
-    guildId: string;
-    description?: string;
-    endTimeInMins?: number;
-    possibleNumberOfWinners: number
+	stillRunning?: Boolean;
+	winnerIds?: Array<string>;
+	participants?: Array<string>
+	messageId: string;
+	guildId: string;
+	description?: string;
+	endTimeInMins?: number;
+	possibleNumberOfWinners: number
 }
